4bdfcb50f8df6f900d5a4b0703c991b670d19927
Steven Fusco <github@stevenfusco.com>
2012-01-19 16:31:51 -0500

    removing __My Company__ from header

--- Twitter+OAuth/Libraries & Headers/OAuthConsumer/OAToken_KeychainExtensions.h
+++ Twitter+OAuth/Libraries & Headers/OAuthConsumer/OAToken_KeychainExtensions.h
@@ -1,9 +1,7 @@
 //
 //  OAToken_KeychainExtensions.h
-//  TouchTheFireEagle
 //
 //  Created by Jonathan Wight on 04/04/08.
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
 //
 
 #import "OAToken.h"
